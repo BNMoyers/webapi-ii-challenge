bbc46400b7a6b9fbde1043e22587da86eab47ce4
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-29 16:12:48 -0500

    post to comments working

--- posts/PostsRouter.js
+++ posts/PostsRouter.js
@@ -1,5 +1,5 @@
 /*dependencies*/
-const express = require('express')
+const express = require("express");
 const router = require("express").Router();
 
 /*imports*/
@@ -7,17 +7,13 @@ const Posts = require("../data/db.js");
 
 /*requests*/
 
-
-
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
 
   !title || !contents
-    ? res
-        .status(400)
-        .json({
-          errorMessage: "Please provide title and contents for the post."
-        })
+    ? res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      })
     : Posts.insert(req.body)
         .then(post => {
           res.status(201).json(post);
@@ -30,4 +26,26 @@ router.post("/", (req, res) => {
         });
 });
 
+router.post("/:id/comments", (req, res) => {
+  const { text, post_id } = req.body;
+
+  !post_id
+    ? res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." })
+    : !text
+    ? res
+        .status(400)
+        .json({ errorMessage: "Please provide text for the comment" })
+    : Posts.insertComment(req.body)
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            error: "There was an error while saving the comment to the database"
+          });
+        });
+});
 module.exports = router;
