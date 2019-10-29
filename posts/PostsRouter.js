399b6c4ba1901dbf6fac1aa07807ced9b8156ab2
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-29 16:18:14 -0500

    get all posts working

--- posts/PostsRouter.js
+++ posts/PostsRouter.js
@@ -48,4 +48,16 @@ router.post("/:id/comments", (req, res) => {
           });
         });
 });
+
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 module.exports = router;
